065688a337779dbeaa763d4053bf006a4e4ae088
Dillon Teakell <teakelld@icloud.com>
2022-09-30 21:06:24 -0400

    Initial Commit

---
+++ WeatherTutorial/WeatherTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTutorialApp.swift
+//  WeatherTutorial
+//
+//  Created by Dillon Teakell on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
